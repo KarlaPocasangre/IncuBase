a8d7d8667ef56abc4796586d8a9d3df05ce774f2
Karla Pocasangre <karlahernandezpocasangre@gmail.com>
2026-05-14 13:54:03 -0600

    style(layout): personalizar scrollbar principal

--- client/src/layouts/MainLayout.jsx
+++ client/src/layouts/MainLayout.jsx
@@ -8,13 +8,16 @@ function MainLayout() {
     <div className="flex h-screen overflow-hidden bg-[#EEF3F0]">
       <Sidebar />
 
-      <div className="flex flex-col flex-1 min-w-0 h-screen overflow-hidden">
+      <div className="flex h-screen min-w-0 flex-1 flex-col overflow-hidden">
         <Topbar />
 
-        <main className="flex-1 min-w-0 overflow-y-auto overflow-x-hidden p-6">
-          <Outlet />
+        <main className="incubase-scroll flex-1 min-w-0 overflow-y-auto overflow-x-hidden">
+          <div className="p-6">
+            <Outlet />
+          </div>
+
+          <Footer />
         </main>
-        <Footer />
       </div>
     </div>
   );
